6d4be97dfa14396ecb8fc2bc3619def089f997d1
fazzyA <faz.pak@gmail.com>
2023-09-24 00:02:30 +0500

    initial structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import Header from '@/components/Layout/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Layout/Footer'
+import Sidebar from '@/components/Layout/SideBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Header />
+      <Sidebar />
+      {children}
+      <Footer />
+      </body>
     </html>
   )
 }
---
+++ src/components/Layout/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Sidebar = () => {
+
+  return (
+    <div>Sidebar</div>
+  );
+};
+
+export default Sidebar;
