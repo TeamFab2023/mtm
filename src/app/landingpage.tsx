db465088e15e87f7ea49912450b0eacf15b5c3ec
‘komalata005’ <Komal.ata005@gmail.comgit config --global user.email Komal.ata005@gmail.com>
2023-10-04 16:15:06 +0500

    hero section

---
+++ src/app/landingpage.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Hero from '@/components/Layout/Hero'
+
+export default function Home() {
+  return (
+    <>
+    {/* <div>Home</div> */}
+    <Hero /></>
+  )
+}
