042daae48e77f2c31fa79b7460c170b579314069
komalata005 <komal.ata005@gmail.com>
2023-10-09 10:07:03 +0500

    features sections

--- src/app/landingpage.tsx
+++ src/app/landingpage.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import Hero from '@/components/Layout/Hero'
+import Footer from '@/components/Layout/Footer'
+import Features from '@/components/Layout/features'
 
 export default function Home() {
   return (
     <>
     {/* <div>Home</div> */}
-    <Hero /></>
+    <Hero />
+    <Features />
+    <Footer />
+    </>
   )
 }
